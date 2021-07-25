d492206d054e6a6ddeadc60370ec0abf1ad63d25
Soups71 <kidsoups@gmail.com>
2021-07-25 13:47:58 -0400

    Allow for dynamic python

--- src/PYTHON/csv_serial.py
+++ src/PYTHON/csv_serial.py
@@ -1,11 +1,12 @@
 import serial
 import csv
 import sys
-USB_PORT = sys.argv[1]
+USB_PORT = str(sys.argv[1])
+FILENAME = str(sys.argv[2])
 ser = serial.Serial(USB_PORT, 9600)
 
 
-with open("values.csv") as value_file:
+with open(FILENAME) as value_file:
     file_reader = csv.reader(value_file, delimeter=',')
     for line in file_reader:
         current_val = line[0]
