8123c663dc3ce667480b3efbff3514ea2680e696
Soups71 <kidsoups@gmail.com>
2021-07-25 14:21:05 -0400

    Updated python code

--- src/PYTHON/csv_serial.py
+++ src/PYTHON/csv_serial.py
@@ -1,14 +1,27 @@
+#!/bin/python3
+# Created by Soups71
+
 import serial
 import csv
 import sys
+
+# Command line parameters
 USB_PORT = str(sys.argv[1])
 FILENAME = str(sys.argv[2])
-ser = serial.Serial(USB_PORT, 9600)
 
 
-with open(FILENAME) as value_file:
-    file_reader = csv.reader(value_file, delimeter=',')
-    for line in file_reader:
-        current_val = line[0]
-        ser.write(current_val.encode())
-        print(f"Value should be {current_val}")
+def main():
+    # Set up serial interface with baud rate of 9600
+    serial_bus = serial.Serial(USB_PORT, 9600)
+
+    # For each line in the csv write the value to the serial bus
+    with open(FILENAME) as value_file:
+        file_reader = csv.reader(value_file, delimeter=',')
+        for line in file_reader:
+            current_val = line[0]
+            # Encode the value to binary
+            serial_bus.write(current_val.encode())
+            print(f"Value should be {current_val}")
+
+if __name__ == "__main__":
+    main()
