ee6294321cd97d39a9f25d20bcec01202a96b940
Soups71 <kidsoups@gmail.com>
2021-07-25 15:34:23 -0400

    More descriptive prompt

--- src/PYTHON/simple_serial.py
+++ src/PYTHON/simple_serial.py
@@ -10,7 +10,7 @@ USB_PORT = str(sys.argv[1])
 serial_bus = serial.Serial(USB_PORT, 9600)
 
 while True:
-    user_input = input("Enter current value: ")
+    user_input = input("Enter current wind speed value: ")
     # Convert the value to binary
     serial_bus.write(user_input.encode())
     print(f"Value should be {user_input}")
