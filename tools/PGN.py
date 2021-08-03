5ca8fa90d13b036afc6b22450570b415c91a190f
Soups71 <kidsoups@gmail.com>
2021-08-02 20:49:46 -0400

    Added search function to find PGN description

--- tools/PGN.py
+++ tools/PGN.py
@@ -3,6 +3,17 @@
 # a program to convert 29 bit extended frame headers in hex to pgns
 # input needs to have stuffed bits removed
 import sys
+import json
+
+
+def pgn_decrpt(found_pgn):
+  file = open("PGN.json", 'r').read()
+  file_data = json.loads(file)
+  pgn_data = file_data["PGNs"]
+  for pgn in pgn_data:
+    if pgn["PGN"] == found_pgn:
+      return pgn["Description"]
+  return "ERROR :: No description was found for the PGN found"
 
 def main():
   num_of_bits = 29
@@ -17,7 +28,7 @@ def main():
   reserve = str(binary_input[3])
   data_page = str(binary_input[4])
   pdu_field = str(binary_input[5:13])
-  group_ext = str(binary_input[14:21])
+  group_ext = str(binary_input[13:21])
 
 # check if PDU Format Field is less than 240
   if int(pdu_field, 2) < 240:
@@ -30,7 +41,7 @@ def main():
   print("The PDU Field Format is: " + pdu_field + " and " + str(int(pdu_field, 2)))
   print("The Group Extension is: " + group_ext)
   print("The PGN is: " + pgn + " and " + str(int(pgn, 2)) + " and " + str(hex(int(pgn, 2))))
-
+  print(f"The PGN found above correlates to {pgn_decrpt(str(int(pgn, 2)))}")
 if __name__ == "__main__":
   input = str(sys.argv[1])
   main()
