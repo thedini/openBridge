bfdab70adf806a29fe3a92b40e266efab5a92f81
Soups71 <kidsoups@gmail.com>
2021-08-05 22:30:10 -0400

    Updated manual pytho script

--- src/PYTHON/simple_serial.py
+++ src/PYTHON/simple_serial.py
@@ -4,13 +4,19 @@
 import serial
 import sys
 
-# Get command line arguments
-USB_PORT = str(sys.argv[1])
+def main():
+    # Get command line arguments
+    USB_PORT = str(sys.argv[1])
 
-serial_bus = serial.Serial(USB_PORT, 9600)
+    serial_bus = serial.Serial(USB_PORT, 9600)
 
-while True:
-    user_input = input("Enter current wind speed value: ")
-    # Convert the value to binary
-    serial_bus.write(user_input.encode())
-    print(f"Value should be {user_input}")
+    while True:
+        speed_input = input("Enter current wind speed value: ")
+        dir_input = input("Enter current wind direction value: ")
+        user_input = f"{speed_input},{dir_input}"
+        # Convert the value to binary
+        serial_bus.write(user_input.encode())
+        print(f"Value should be {user_input}")
+
+if __name__ == "__main__":
+    main()
