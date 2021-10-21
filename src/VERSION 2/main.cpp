7b35612b0ec91f20a6411f0fceebd0021baa601b
Soups71 <kidsoups@gmail.com>
2021-10-20 23:04:07 -0400

    Abstracted out the que to make message sending more uniform

--- src/VERSION 2/main.cpp
+++ src/VERSION 2/main.cpp
@@ -14,33 +14,30 @@
 #include <Windows.h>
 
 std::string convertToString(std::vector<std::string> &data);
+int add_message(std::vector<std::string> &data, std::vector<std::vector<std::string>> &queued_messages);
+void send_msg(std::vector<std::vector<std::string>> &queued_messages, Serial &serial_interface, bool verbose=true);
 int main()
 {
-    char vector_data[300];
     int wind_speed = 0;
     int times = 0;
     int i = 0;
-
-    // Initialize the serial object
+    std::vector<std::vector<std::string>> queued_messages;
     Serial serial_port = Serial("COM4");
     serial_port.setupParams(115200, 8, 0, 0);
-
+    std::vector<std::string> indi_message;
     while ( i < 100000) {
         wind_speed = (wind_speed % 20) + 1;
         times = (times % 2) + 1;
-        std::vector<std::string> message;
-        SetN2kWindSpeed(message, 1, wind_speed, 20, 2);
-        std::string csv_data = convertToString(message);
-        printf("%s\n", csv_data.c_str());
-        serial_port.sendData(csv_data);
-        Sleep(100);
-        std::vector<std::string> message2;
-        SetN2kWaterDepth(message2, 0, wind_speed / 3.3, 0, 1000);
-        std::string csv_data2 = convertToString(message2);
-        printf("%s\n", csv_data2.c_str());
-        serial_port.sendData(csv_data2);
+        SetN2kWindSpeed(indi_message, 1, wind_speed, 20, 2);
+        add_message(indi_message, queued_messages);
+        indi_message.clear();
+        SetN2kWaterDepth(indi_message, 0, wind_speed / 3.3, 0, 1000);
+        add_message(indi_message, queued_messages);
+        indi_message.clear();
+        send_msg(queued_messages, serial_port, 1);
         Sleep(1000);
         i++;
+
     }
     serial_port.close_handle();
     return 0;
@@ -53,4 +50,30 @@ std::string convertToString(std::vector<std::string> &data){
     }
     csv_string += "=";
     return csv_string;
+}
+int add_message(std::vector<std::string> &data, std::vector<std::vector<std::string>> &queued_messages){
+    std::string uid = data.front();
+    int idx = 0;
+    for(auto x: queued_messages) {
+        if(x.front() == uid){
+            queued_messages.at(idx) = data;
+            return 1;
+        }
+        idx++;
+    }
+    queued_messages.push_back(data);
+    return 0;
+
+}
+void send_msg(std::vector<std::vector<std::string>> &queued_messages, Serial &serial_interface, bool verbose){
+    std::string data_str;
+    for(auto x: queued_messages) {
+        data_str = convertToString(x);
+        if(verbose){
+            printf("%s\n", data_str.c_str());
+        }
+        serial_interface.sendData(data_str);
+
+        Sleep(100);
+    }
 }
