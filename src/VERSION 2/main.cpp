12928ee26f0d4f4da8271962799bc8acae18cc25
Soups71 <kidsoups@gmail.com>
2021-10-24 23:12:58 -0400

    Changed up the main function to utilize threading

--- src/VERSION 2/main.cpp
+++ src/VERSION 2/main.cpp
@@ -4,50 +4,69 @@
  * documentation is currently being written for this project and the code base
  * is being tweaked daily.
  *
- * Last Updated: 10/20/21
+ * Same as the version 2 branch but I tried to add some threading
+ *
+ * Last Updated: 10/24/21
  */
 #include<windows.h>
 #include <vector>
 #include<stdio.h>
 #include "windows_Serial.h"
 #include "N2kmessage.h"
+#include <thread>
+#include <mutex>
 #include <Windows.h>
 
 std::string convertToString(std::vector<std::string> &data);
-int add_message(std::vector<std::string> &data, std::vector<std::vector<std::string>> &queued_messages);
-void send_msg(std::vector<std::vector<std::string>> &queued_messages, Serial &serial_interface, bool verbose=true);
+bool add_message(std::vector<std::string> &data);
+void send_msg(bool verbose=true);
+void craft_msg();
+
+
+std::mutex locker;
+Serial serial_port = Serial("COM27");
+std::vector<std::vector<std::string>> queued_messages;
 
 
 int main()
 {
-    int wind_speed = 0;
-    int times = 0;
-    int i = 0;
-    std::vector<std::vector<std::string>> queued_messages;
-    Serial serial_port = Serial("COM4");
     serial_port.setupParams(115200, 8, 0, 0);
-    std::vector<std::string> indi_message;
+    std::thread crafter(craft_msg);
+    std::thread sender(send_msg, true);
 
+    crafter.join();
+    sender.join();
 
-    while ( i < 100000) {
-        wind_speed = (wind_speed % 20) + 1;
-        times = (times % 2) + 1;
-        SetN2kWindSpeed(indi_message, 1, wind_speed, 20, 2);
-        add_message(indi_message, queued_messages);
-        indi_message.clear();
-        SetN2kWaterDepth(indi_message, 0, wind_speed / 3.3, 0, 1000);
-        add_message(indi_message, queued_messages);
-        indi_message.clear();
-        send_msg(queued_messages, serial_port, 1);
-        Sleep(1000);
-        i++;
-
-    }
     serial_port.close_handle();
     return 0;
 }
 
-// Used to conver the vector into a CSV that the teensy will be able to parse.
+
+void craft_msg(){
+    std::vector<std::string> indi_message;
+    int wind_speed = 0;
+    int times = 0;
+    int i = 0;
+    while ( i < 10) {
+        //printf("%d\n", i);
+        wind_speed = (wind_speed % 20) + 1;
+        times = (times % 2) + 1;
+        SetN2kWindSpeed(indi_message, 1, wind_speed, 20, 2);
+        add_message(indi_message);
+        indi_message.clear();
+        SetN2kWaterDepth(indi_message, 0, wind_speed / 3.3, 0, 1000);
+        add_message(indi_message);
+        indi_message.clear();
+        //send_msg(queued_messages, serial_port, 1);
+        Sleep(1000);
+        i++;
+
+    }
+}
+
+
+
+
 std::string convertToString(std::vector<std::string> &data){
     std::string csv_string = "";
     for(auto x: data) {
@@ -56,33 +75,52 @@ std::string convertToString(std::vector<std::string> &data){
     csv_string += "=";
     return csv_string;
 }
-
-// Updates queued messages in order to prevent duplicates.
-int add_message(std::vector<std::string> &data, std::vector<std::vector<std::string>> &queued_messages){
+bool add_message(std::vector<std::string> &data){
     std::string uid = data.front();
+    locker.lock();
+    std::vector<std::vector<std::string>> current_vect = queued_messages;
+    locker.unlock();
     int idx = 0;
-    for(auto x: queued_messages) {
+    bool updated = false;
+    for(auto x: current_vect) {
         if(x.front() == uid){
-            queued_messages.at(idx) = data;
-            return 1;
+            //printf("Messege updated in queue\n");
+            current_vect.at(idx) = data;
+            updated = true;
         }
         idx++;
     }
-    queued_messages.push_back(data);
-    return 0;
+    if(!updated) {
+        //printf("New Messege added to queue\n");
+        current_vect.push_back(data);
+    }
+    locker.lock();
+    queued_messages = current_vect;
+    locker.unlock();
+    return updated;
 
 }
 
-// Sends the queued messages.
-void send_msg(std::vector<std::vector<std::string>> &queued_messages, Serial &serial_interface, bool verbose){
-    std::string data_str;
-    for(auto x: queued_messages) {
-        data_str = convertToString(x);
-        if(verbose){
-            printf("%s\n", data_str.c_str());
-        }
-        serial_interface.sendData(data_str);
 
-        Sleep(100);
+void send_msg(bool verbose){
+
+    for(int i = 0; i<100; i++) {
+
+        std::string data_str;
+        locker.lock();
+        std::vector<std::vector<std::string>> raw_vec = queued_messages;
+        locker.unlock();
+        int vec_length = raw_vec.size();
+        printf("%d\n", vec_length);
+        for (int idx = 0; idx < vec_length; idx++) {
+
+            data_str = convertToString(raw_vec[idx]);
+            //printf("%s\n", data_str.c_str());
+            if (verbose) {
+                printf("%s\n", data_str.c_str());
+            }
+            serial_port.sendData(data_str);
+            Sleep(100);
+        }
     }
 }
